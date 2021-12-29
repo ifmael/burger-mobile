c42c2b90fcfb1e6e27ac4331b921f9d2b2dbd915
ismael Rodriguez <ismael.rm.1986@gmail.com>
2021-12-29 19:47:29 +0100

    chore(config): :wrench: config project

---
+++ app.config.ts
@@ -0,0 +1,42 @@
+import { ExpoConfig } from "@expo/config-types";
+import "dotenv/config";
+
+// const config: ExpoConfig = {
+//   name: "my-app",
+// };
+
+// export default config;
+export default ({ config }: { config: ExpoConfig }) => {
+    return {
+        ...config,
+        // hooks: {
+        //   postPublish: [
+        //     {
+        //       file: "sentry-expo/upload-sourcemaps",
+        //       config: {
+        //         organization: process.env.SENTRY_ORG,
+        //         project: process.env.SENTRY_PROJECT,
+        //         authToken: process.env.SENTRY_AUTH_TOKEN,
+        //         setCommits: true,
+        //       },
+        //     },
+        //   ],
+        // },
+        // All values in extra will be passed to your app.
+
+        extra: {
+            releaseChannel: process.env.RELEASE_CHANNEL,
+            server: {
+                local: process.env.EXPO_LOCAL_SERVER,
+                production: process.env.EXPO_PRODUCTION_SERVER,
+            },
+            secret: {
+                development: process.env.EXPO_JWT_SECRET_DEVELOPMENT,
+                production: process.env.EXPO_JWT_SECRET_DEVELOPMENT,
+            },
+            // mapsToken: process.env.GOOGLE_MAPS_KEY,
+            // placesToken: process.env.PLACES_API,
+        },
+        plugins: ["expo-updates"],
+    };
+};
